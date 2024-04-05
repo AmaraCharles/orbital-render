52b0ddd3a927bffe2b7864fba202625e9ae85c1f
AmaraCharles <falsepegasus@gmail.com>
2024-04-05 04:28:34 +0100

    dob and number

--- stoxmirror-server-main/routes/auth/register.js
+++ stoxmirror-server-main/routes/auth/register.js
@@ -64,7 +64,7 @@ router.post("/register", async (req, res) => {
       transactions: [],
       withdrawals: [],
       planHistory: [],
-      ohine,
+      phone,
       DOB,
      
       accounts: {
